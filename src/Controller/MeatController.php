c51a6ba8b94e7c7695ec3dd3166b48fe2701d5c3
Samy <chebreksamy@gmail.com>
2023-12-13 21:19:22 +0100

    Creation of meatcontroller.

---
+++ src/Controller/MeatController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MeatController extends AbstractController
+{
+//    #[Route('/viande', name: 'app_viande')]
+//    public function showViande(ManagerRegistry $mr): Response
+//    {
+//        $ids = [1, 2, 3, 4, 5]; // Remplacez ce tableau par les ID que vous souhaitez récupérer
+//        $viandes = $mr->getRepository(Viande::class)->findBy(['id' => $ids]);
+//
+//        foreach ($viandes as $viande) {
+//            $name = $viande->getName();
+//            $description = $viande->getDescription();
+//
+//        }
+//
+//        // Assurez-vous de retourner une réponse appropriée ici
+//    }
+
+}
